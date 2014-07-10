68a2e1b69baa5a39c9639f00181d17f2e22b0a52
marinfischer <mfischer@thecodeboutique.com>
2014-07-10 13:00:55 -0700

    first commit

---
+++ TableViewProgrammatically/TableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  TableViewController.swift
+//  TableViewProgrammatically
+//
+//  Created by Marin Fischer on 7/10/14.
+//  Copyright (c) 2014 MarinFischer. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        // Custom initialization
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // #pragma mark - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
